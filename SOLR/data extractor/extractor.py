26b7d0e882a91fa1fd680ed4d97f35d56e8b9e2d
Tatsianenka Ilya <itiv422@gmail.com>
2019-08-22 21:32:12 +0300

    refactored extractor.py

--- SOLR/data extractor/extractor.py
+++ SOLR/data extractor/extractor.py
@@ -2,12 +2,14 @@ import pysolr
 import epub
 import os
 
-solr = pysolr.Solr('http://localhost:8983/solr/', timeout=10, auth=<type of authentication>)
+book_solr_connection = pysolr.Solr('http://localhost:8983/solr/book', timeout=10)
+cover_solr_connection = pysolr.Solr('http://localhost:8983/solr/cover', timeout=10)
 epub_data_directory = 'data'
 data_directory_files = os.listdir(epub_data_directory)
 epub_directory_files = filter(lambda x: x.endswith('.epub'), data_directory_files) 
 
 for epub_file in epub_directory_files:
+    print(epub_file)
     book = epub.open(epub_data_directory + '/' +epub_file)
     title, creator, date = '', '', ''
     if len(book.opf.metadata.titles) > 0:
@@ -15,25 +17,22 @@ for epub_file in epub_directory_files:
     if len(book.opf.metadata.creators) > 0:
         creator, _, _ = book.opf.metadata.creators[0]
     if len(book.opf.metadata.dates) > 0:
-        date = book.opf.metadata.dates[0]
+        date = book.opf.metadata.dates[0][0]
     description = book.opf.metadata.description
-    solr.add({
+    cover_solr_connection.add([{
         'title': title,
         'creator': creator,
         'date': date,
         'description': description,
         'file': epub_file
-    })
-
-    for item_id, linear in book.opf.spine.itemrefs:
+    }])
+    for item_id, _ in book.opf.spine.itemrefs:
         item = book.get_item(item_id)
-        if linear:
-            print('Linear item ' + item.href)
-        else:
-            print( 'Non-linear item ' + item.href)
         data = book.read_item(item)
-        solr.add({
+        book_solr_connection.add([{
             'file': title,
             'page': item_id,
-            'cantent': content
-        })
+            'content': data
+        }])
+book_solr_connection.commit()
+cover_solr_connection.commit()
